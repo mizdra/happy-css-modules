0c1fe232a9639f5482ea93b590716caa7bdc5ec9
mizdra <pp.mizdra@gmail.com>
2022-06-12 22:39:06 +0900

    build: lint `*.js`

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,15 @@ module.exports = {
   root: true,
   overrides: [
     {
-      files: ['*.{ts,tsx,cts,mts}'],
-      extends: ['@mizdra/mizdra', '@mizdra/mizdra/+typescript', '@mizdra/mizdra/+prettier'],
+      files: ['**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}'],
+      extends: ['@mizdra/mizdra', '@mizdra/mizdra/+prettier'],
       env: {
         node: true,
       },
+    },
+    {
+      files: ['*.{ts,tsx,cts,mts}'],
+      extends: ['@mizdra/mizdra/+typescript', '@mizdra/mizdra/+prettier'],
     },
     {
       files: ['test/**/*.{ts,tsx,cts,mts}'],
