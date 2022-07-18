4df8ec08d44026c5486d998f8e7f7872f9cab83e
mizdra <pp.mizdra@gmail.com>
2022-07-18 23:59:11 +0900

    ci: add tsconfig path setting to development tools

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,11 @@ const config = {
   testPathIgnorePatterns: ['/node_modules/', '\\.d\\.ts$', 'lib/', 'example/', 'coverage/'],
   watchPathIgnorePatterns: ['\\.d\\.ts$'],
   moduleFileExtensions: ['js', 'ts', 'json'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
 };
 
 module.exports = config;
