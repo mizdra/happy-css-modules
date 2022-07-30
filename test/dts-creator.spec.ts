023ea264b31e864617319072cd642eeaf6e5874d
mizdra <pp.mizdra@gmail.com>
2022-07-30 22:57:05 +0900

    refactor: refactor test

--- test/dts-creator.spec.ts
+++ test/dts-creator.spec.ts
@@ -1,11 +1,18 @@
+import { readFile } from 'fs/promises';
 import { DtsCreator } from '../src/dts-creator';
 
 describe('DtsContent', () => {
   describe('#writeFile', () => {
     it('writes a file', async () => {
-      await new DtsCreator().create('test/testStyle.css').then(async (content) => {
-        return content.writeFile();
-      });
+      const content = await new DtsCreator().create('test/testStyle.css');
+      await content.writeFile();
+      expect(await readFile('test/testStyle.css.d.ts', 'utf8')).toMatchInlineSnapshot(`
+        "declare const styles: {
+          readonly \\"myClass\\": string;
+        };
+        export = styles;
+        "
+      `);
     });
   });
 });
--- test/testStyle.css.d.ts
+++
@@ -1,4 +0,0 @@
-declare const styles: {
-  readonly "myClass": string;
-};
-export = styles;
