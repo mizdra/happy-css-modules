99c23cf24cccb1adfe34c2d99ac0c40a3a7466dd
mizdra <pp.mizdra@gmail.com>
2022-08-06 18:22:45 +0900

    feat: implement the prototype of loader

---
+++ src/loader.ts
@@ -0,0 +1,111 @@
+import { readFile, stat } from 'fs/promises';
+import postcss from 'postcss';
+import { generateOriginalPosition, generateLocalTokenNames, parseAtImport, Position, walkByMatcher } from './postcss';
+
+/** The value returned from the transformer. */
+export type TransformResult = {
+  /** The transformed code. */
+  css: string;
+  /** The source map from the transformed code to the original code. */
+  map?: string;
+};
+
+/** The function to transform source code. */
+export type Transformer = (source: string, from: string) => TransformResult | Promise<TransformResult>;
+
+/** The exported token. */
+export type Token = {
+  /** The token name. */
+  name: string;
+  /**
+   * The original positions of the token in the source file.
+   * Cascading tokens may have multiple positions.
+   */
+  originalPositions: Position[];
+};
+
+type CacheEntry = {
+  mtime: number;
+  result: LoadResult;
+};
+
+/** The result of `Loader#load`. */
+export type LoadResult = {
+  /** The file path of the source file. */
+  filePath: string;
+  /** The stylesheets imported by the source file. */
+  importedSheets: LoadResult[];
+  /** The tokens exported by the source file. */
+  localTokens: Token[];
+};
+
+/**
+ * A class inspired by `css-modules-loader-core` to collect information about css modules.
+ */
+export class Loader {
+  private cache: Map<string, CacheEntry> = new Map();
+  async load(filePath: string, transform?: Transformer): Promise<LoadResult> {
+    // If cache available, return it.
+    // TODO: use `@file-cache/core`
+    const mtime = (await stat(filePath)).mtime.getTime();
+    const cacheEntry = this.cache.get(filePath);
+    if (cacheEntry && cacheEntry.mtime === mtime) return cacheEntry.result;
+
+    let css = await readFile(filePath, 'utf-8');
+    let map: string | undefined;
+    if (transform) {
+      const result = await transform(css, filePath);
+      css = result.css;
+      map = result.map;
+    }
+
+    const ast = postcss.parse(css, { from: filePath, map: { inline: false, prev: map ?? true } });
+
+    // Get the local tokens exported by the source file.
+    // The tokens are fetched using `postcss-modules` plugin.
+    const localTokenNames = await generateLocalTokenNames(ast);
+
+    const importedSheetPaths: string[] = [];
+    const localTokens: Token[] = [];
+
+    walkByMatcher(ast, {
+      // Collect the sheets imported by `@import` rule.
+      atImport: (atImport) => {
+        const importedSheetPath = parseAtImport(atImport);
+        if (importedSheetPath) importedSheetPaths.push(importedSheetPath);
+      },
+      // Traverse the source file to find a class selector that matches the local token.
+      classSelector: (rule, classSelector) => {
+        // Consider a class selector to be the origin of a token if it matches a token fetched by postcss-modules.
+        // NOTE: This method has false positives. However, it works as expected in many cases.
+        if (!localTokenNames.includes(classSelector.value)) return;
+
+        const originalPosition = generateOriginalPosition(rule, classSelector);
+
+        const localToken = localTokens.find((token) => token.name === classSelector.value);
+        if (localToken) {
+          localToken.originalPositions.push(originalPosition);
+        } else {
+          localTokens.push({
+            name: classSelector.value,
+            originalPositions: [originalPosition],
+          });
+        }
+      },
+    });
+
+    // Load imported sheets recursively.
+    const importedSheets: LoadResult[] = [];
+    for (const importedSheetPath of importedSheetPaths) {
+      importedSheets.push(await this.load(importedSheetPath));
+    }
+
+    const result: LoadResult = {
+      filePath,
+      importedSheets,
+      localTokens,
+    };
+    this.cache.set(filePath, { mtime, result });
+    return result;
+  }
+}
---
+++ src/postcss.ts
@@ -0,0 +1,115 @@
+import postcss, { Rule, AtRule, Root } from 'postcss';
+import modules from 'postcss-modules';
+import selectorParser, { ClassName } from 'postcss-selector-parser';
+import valueParser from 'postcss-value-parser';
+
+/** The position of node. */
+export type Position = {
+  filePath: string;
+  /** The line number in the source file. It is 1-based. */
+  line?: number;
+  /** The column number in the source file. It is 0-based. */
+  column?: number;
+};
+
+/**
+ * The name of exported token by the source file excluding the imported from other sheets.
+ * @param ast The root node of the source file.
+ * @returns The exported token names.
+ */
+export async function generateLocalTokenNames(ast: Root): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    postcss()
+      .use(
+        modules({
+          getJSON: (_cssFileName, json) => {
+            resolve(Object.keys(json));
+          },
+        }),
+      )
+      .process(ast)
+      .catch(reject);
+  });
+}
+
+/**
+ * Generate the token's position on the source file.
+ * @param rule The rule node that contains the token.
+ * @param classSelector The class selector node that contains the token.
+ * @returns The token's position on the source file.
+ */
+export function generateOriginalPosition(rule: Rule, classSelector: ClassName): Position {
+  // The node derived from `postcss.parse` always has `source` property. Therefore, this line is unreachable.
+  if (rule.source === undefined || classSelector.source === undefined) throw new Error('Node#source is undefined');
+  // The node derived from `postcss.parse` always has `start` property. Therefore, this line is unreachable.
+  if (rule.source.start === undefined || classSelector.source.start === undefined)
+    throw new Error('Node#start is undefined');
+
+  return {
+    filePath: rule.source.input.file,
+    // The line is 1-based.
+    // TODO: If `source` contains an inline sourcemap, use the sourcemap to get the line and column.
+    // This allows support for scss and less users.
+    line: rule.source.start.line + classSelector.source.start.line - 1,
+    // Postcss's column is 1-based but our column is 0-based.
+    column: rule.source.start.column - 1 + (classSelector.source.start.column - 1),
+  };
+}
+
+type Matcher = {
+  atImport: (atImport: AtRule) => void;
+  classSelector: (rule: Rule, classSelector: ClassName) => void;
+};
+
+function isAtRuleNode(node: postcss.Node): node is AtRule {
+  return node.type === 'atrule';
+}
+
+function isAtImportNode(node: postcss.Node): node is AtRule {
+  return isAtRuleNode(node) && node.name === 'import';
+}
+
+function isRuleNode(node: postcss.Node): node is Rule {
+  return node.type === 'rule';
+}
+
+/**
+ * Walk the AST and call the matcher functions.
+ * @param ast The AST to walk.
+ * @param matcher The matcher functions to call.
+ */
+export function walkByMatcher(ast: Root, matcher: Matcher): void {
+  ast.walk((node) => {
+    if (isAtImportNode(node)) {
+      matcher.atImport(node);
+    } else if (isRuleNode(node)) {
+      // In `rule.selector` comes the following string:
+      // 1. ".foo"
+      // 2. ".foo:hover"
+      // 3. ".foo, .bar"
+      selectorParser((selectors) => {
+        selectors.walk((selector) => {
+          if (selector.type === 'class') {
+            // In `selector.value` comes the following string:
+            // 1. "foo"
+            // 2. "bar"
+            matcher.classSelector(node, selector);
+          }
+        });
+      }).processSync(node);
+    }
+  });
+}
+
+/**
+ * Parse the `@import` rule.
+ * @param atImport The `@import` rule to parse.
+ * @returns The imported sheet path.
+ */
+export function parseAtImport(atImport: AtRule): string | undefined {
+  const firstNode = valueParser(atImport.params).nodes[0];
+  if (firstNode.type === 'string') return firstNode.value;
+  if (firstNode.type === 'function' && firstNode.value === 'url') {
+    if (firstNode.nodes[0].type === 'string') return firstNode.nodes[0].value;
+  }
+}
