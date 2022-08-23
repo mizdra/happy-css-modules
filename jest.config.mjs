4e2cd25240c3e2e079563d9fa1d17bb15700c2c9
mizdra <pp.mizdra@gmail.com>
2022-08-24 00:34:11 +0900

    build: use babel instead of ts-jest in jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,23 +2,16 @@
 
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-  preset: 'ts-jest/presets/default-esm',
+  // for ESM
+  // @ts-expect-error
+  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     // Map `./**/xxx.js` to `./**/xxx` (for ESM)
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  // do not transform `import` statements (for ESM)
-  transform: {},
   testMatch: ['<rootDir>/src/**/*.test.ts?(x)'],
   moduleFileExtensions: ['js', 'ts', 'json'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
-  globals: {
-    // TODO: Use babel-jest
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-      useESM: true,
-    },
-  },
 };
 
 // eslint-disable-next-line import/no-default-export
