badaa5f0e42535c3336f9b49cbd9220f0c65bf9d
mizdra <pp.mizdra@gmail.com>
2022-08-24 00:25:54 +0900

    fix: make jest ESM-ready

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,16 +2,21 @@
 
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
+  moduleNameMapper: {
+    // Map `./**/xxx.js` to `./**/xxx` (for ESM)
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  // do not transform `import` statements (for ESM)
+  transform: {},
   testMatch: ['<rootDir>/src/**/*.test.ts?(x)'],
   moduleFileExtensions: ['js', 'ts', 'json'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
   globals: {
-    // TODO: Use ESM + babel-jest
+    // TODO: Use babel-jest
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
+      useESM: true,
     },
   },
 };
