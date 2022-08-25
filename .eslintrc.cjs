c4f4f3576938fbac63bc8957ef728662422c5fea
mizdra <pp.mizdra@gmail.com>
2022-08-26 01:03:20 +0900

    build: use `import/no-internal-modules` rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,16 @@
 /** @type import('eslint').Linter.BaseConfig */
 module.exports = {
   root: true,
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        project: ['./tsconfig.src.json', './tsconfig.test.json'],
+      },
+    },
+  },
   overrides: [
     {
       files: ['**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}'],
@@ -11,10 +21,19 @@ module.exports = {
         node: true,
       },
       rules: {
-        'import/no-extraneous-dependencies': 'error',
-        // disable because this rule do not support ESM in TypeScript.
-        // ref: https://github.com/import-js/eslint-plugin-import/issues/2170
-        'import/no-unresolved': 'off',
+        'import/no-internal-modules': [
+          'error',
+          {
+            allow: [
+              '**/util.js',
+              '**/loader/index.js',
+              '**/emitter/index.js',
+              '**/library/*/index.js',
+              '**/dist/index.js',
+              'yargs/*',
+            ],
+          },
+        ],
       },
     },
     {
