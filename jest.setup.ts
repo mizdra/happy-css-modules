9c53c7787da6c2b0fd0a2df9a292e6def237a47b
mizdra <pp.mizdra@gmail.com>
2022-08-27 13:53:37 +0900

    test: remove unused workaround

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,49 +1,7 @@
-import {
-  toMatchInlineSnapshot,
-  toMatchSnapshot,
-  toThrowErrorMatchingInlineSnapshot,
-  toThrowErrorMatchingSnapshot,
-} from 'jest-snapshot';
-import mock from 'mock-fs';
 import { format } from 'prettier';
 // eslint-disable-next-line no-restricted-imports
 import { removeFixtures } from './src/test/util.js';
 
-// There is a problem that snapshots cannot be written when the filesystem is mocked with mock-fs.
-// Here is a workaround for that problem by overriding the default matcher.
-// ref: https://github.com/tschaub/mock-fs#using-with-jest-snapshot-testing
-// TODO: open an issue on tschaub/mock-fs
-expect.extend({
-  toMatchInlineSnapshot(...args: Parameters<typeof toMatchInlineSnapshot>) {
-    // @ts-expect-error
-    return mock.bypass(() => toMatchInlineSnapshot.call(this, ...args));
-  },
-  toMatchSnapshot(...args) {
-    // @ts-expect-error
-    return mock.bypass(() => toMatchSnapshot.call(this, ...args));
-  },
-  toThrowErrorMatchingInlineSnapshot(...args) {
-    // @ts-expect-error
-    return mock.bypass(() => toThrowErrorMatchingInlineSnapshot.call(this, ...args));
-  },
-  toThrowErrorMatchingSnapshot(...args) {
-    // @ts-expect-error
-    return mock.bypass(() => toThrowErrorMatchingSnapshot.call(this, ...args));
-  },
-});
-const nativeConsoleLog = console.log;
-const nativeConsoleWarn = console.warn;
-const nativeConsoleError = console.error;
-const nativeConsoleInfo = console.error;
-const nativeConsoleDebug = console.error;
-console.log = (...args) => mock.bypass(() => nativeConsoleLog.call(console, ...args));
-console.warn = (...args) => mock.bypass(() => nativeConsoleWarn.call(console, ...args));
-console.error = (...args) => mock.bypass(() => nativeConsoleError.call(console, ...args));
-console.info = (...args) => mock.bypass(() => nativeConsoleInfo.call(console, ...args));
-console.debug = (...args) => mock.bypass(() => nativeConsoleDebug.call(console, ...args));
-
-// Mocking by mock-fs prevents jest from reporting test results. Therefore, un-mock before the test is finished.
-afterEach(() => mock.restore());
 afterEach(() => removeFixtures());
 
 const jsonSerializer: jest.SnapshotSerializerPlugin = {
