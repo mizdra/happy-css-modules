fc2e1db765f3b76765f5267ead6e0556c5f6f8af
mizdra <pp.mizdra@gmail.com>
2022-09-10 23:19:36 +0900

    refactor: refactor resolver tests

--- src/resolver/index.test.ts
+++ src/resolver/index.test.ts
@@ -1,26 +1,17 @@
-import dedent from 'dedent';
-import { Loader } from '../loader/index.js';
 import { createFixtures, getFixturePath } from '../test/util.js';
 import { createDefaultResolver } from './index.js';
 
-const loader = new Loader({ resolver: createDefaultResolver() });
+const defaultResolver = createDefaultResolver();
+const request = getFixturePath('/test/1.css');
 
 test('resolve with webpackResolver when other resolvers fail to resolve', async () => {
   createFixtures({
-    '/test/1.css': dedent`
-    @import '2.css';
-    @import '3.css';
-    @import '~4.css';
-    `,
     '/test/2.css': `.a {}`,
     '/test/3.css': `.a {}`,
     '/node_modules/3.css/index.css': `.a {}`,
     '/node_modules/4.css/index.css': `.a {}`,
   });
-  const result = await loader.load(getFixturePath('/test/1.css'));
-  expect(result.dependencies).toStrictEqual([
-    getFixturePath('/test/2.css'),
-    getFixturePath('/test/3.css'),
-    getFixturePath('/node_modules/4.css/index.css'),
-  ]);
+  expect(await defaultResolver('2.css', { request })).toBe(getFixturePath('/test/2.css'));
+  expect(await defaultResolver('3.css', { request })).toBe(getFixturePath('/test/3.css'));
+  expect(await defaultResolver('~4.css', { request })).toBe(getFixturePath('/node_modules/4.css/index.css'));
 });
