74f6be5f89bc227adb274a07d055720fea4bfe3a
mizdra <pp.mizdra@gmail.com>
2022-09-11 17:21:51 +0900

    build: improve eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
       env: {
         node: true,
       },
+      reportUnusedDisableDirectives: true,
       rules: {
         // disable because this rule do not support ESM in TypeScript.
         // ref: https://github.com/import-js/eslint-plugin-import/issues/2170
@@ -47,7 +48,7 @@ module.exports = {
       },
     },
     {
-      files: ['test/**/*.{ts,tsx,cts,mts}'],
+      files: ['src/test/**/*.{ts,tsx,cts,mts}', '*.test.{ts,tsx,cts,mts}'],
       env: {
         jest: true,
       },
