f7a31a69d9a4806f426f95acc1814d84a9656896
mizdra <pp.mizdra@gmail.com>
2022-09-17 23:33:14 +0900

    feat: support resolving URL in relative-resolver

--- src/resolver/relative-resolver.ts
+++ src/resolver/relative-resolver.ts
@@ -1,18 +1,28 @@
-import { dirname, resolve } from 'path';
-import { fileURLToPath, pathToFileURL } from 'url';
+import fetch from 'node-fetch';
 import { exists } from '../util.js';
 import type { Resolver } from './index.js';
 
-export const createRelativeResolver: () => Resolver = () => async (specifier, options) => {
-  // ignore `http(s)://`
-  if (!options.request.startsWith('file://')) return false;
+export const createRelativeResolver: () => Resolver =
+  () =>
+  async (specifier, { request }) => {
+    let url: URL;
+    // Check if it can be converted to a URL
+    try {
+      url = new URL(specifier, request);
+    } catch (e) {
+      return false;
+    }
 
-  const resolvedPath = resolve(dirname(fileURLToPath(options.request)), specifier);
-  const isExists = await exists(resolvedPath);
-  if (isExists) {
-    const resolvedFileURL = pathToFileURL(resolvedPath).href;
-    return resolvedFileURL;
-  } else {
+    // If the resource does not exist, it must fallback to another resolver.
+    // Therefore, the existence of the resource is checked.
+    if (url.protocol === 'file:') {
+      // Check if the resource exists on the file system
+      const isExists = await exists(url.pathname);
+      if (isExists) return url.href;
+    } else if (url.protocol === 'http:' || url.protocol === 'https:') {
+      // Check if the resource exists on the web
+      const res = await fetch(url.href, { method: 'HEAD' });
+      if (res.ok) return url.href;
+    }
     return false;
-  }
-};
+  };
