50d5456cadc74d1d6a03d044292217ed3472245b
mizdra <pp.mizdra@gmail.com>
2022-09-18 13:05:01 +0900

    test: ESM edition of msw does not work in ESM environment, so import CJS edition
    - ref: https://github.com/mswjs/msw/pull/1399

--- src/test/msw.ts
+++ src/test/msw.ts
@@ -1,4 +1,11 @@
+import { createRequire } from 'node:module';
 import type { SetupServerApi } from 'msw/node';
-import { setupServer } from 'msw/node';
+
+const require = createRequire(import.meta.url);
+
+// NOTE: ESM edition of msw does not work in ESM environment, so import CJS edition
+// ref: https://github.com/mswjs/msw/pull/1399
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+const { setupServer } = require('msw/node') as typeof import('msw/node');
 
 export const server: SetupServerApi = setupServer();
