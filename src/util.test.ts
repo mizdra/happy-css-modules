58b672ac60276d113feefd7a25d7c9a61464776f
mizdra <pp.mizdra@gmail.com>
2022-09-22 01:02:02 +0900

    test: add tests for utilities

--- src/util.test.ts
+++ src/util.test.ts
@@ -1,7 +1,11 @@
+import { stat } from 'fs/promises';
 import { join } from 'path';
-import { hasProp, isObject, isSystemError, unique, uniqueBy } from '../src/util.js';
-import { createFixtures, getFixturePath } from './test/util.js';
-import { isMatchByGlob, exists } from './util.js';
+import { pathToFileURL } from 'url';
+import { rest } from 'msw';
+import { fetchContent, hasProp, isObject, isSystemError, unique, uniqueBy } from '../src/util.js';
+import { server } from './test/msw.js';
+import { createFixtures, getFixturePath, oneOf } from './test/util.js';
+import { isURL, fetchRevision, isMatchByGlob, exists } from './util.js';
 
 function fakeSystemError({ code }: { code: string }) {
   const error = new Error();
@@ -86,4 +90,54 @@ test('isMatchByGlob', () => {
   expect(isMatchByGlob(join(cwd, '1.css'), '!(*.css)', { cwd })).toBe(false);
   expect(isMatchByGlob(join(cwd, '1.scss'), '!(*.css)', { cwd })).toBe(true);
   expect(isMatchByGlob(join(cwd, '1.less'), '!(*.css)', { cwd })).toBe(true);
+});
+
+describe('fetchRevision', () => {
+  test('http/https protocol', async () => {
+    const protocol = oneOf(['http', 'https']);
+    server.use(
+      rest.get(`${protocol}://example.com/test/etag.css`, (_req, res, ctx) =>
+        res(ctx.set('etag', 'etag-value'), ctx.set('last-modified', '1234567890'), ctx.text('.a {}')),
+      ),
+    );
+    server.use(
+      rest.get(`${protocol}://example.com/test/last-modified.css`, (_req, res, ctx) =>
+        res(ctx.set('last-modified', '1234567890'), ctx.text('.a {}')),
+      ),
+    );
+    server.use(rest.get(`${protocol}://example.com/test/content-hash.css`, (_req, res, ctx) => res(ctx.text('.a {}'))));
+    expect(await fetchRevision(`${protocol}://example.com/test/etag.css`)).toBe('etag:etag-value');
+    expect(await fetchRevision(`${protocol}://example.com/test/last-modified.css`)).toBe('last-modified:1234567890');
+    expect(await fetchRevision(`${protocol}://example.com/test/content-hash.css`)).toBe(
+      'content-hash:16106342b3ea064b29ae5c92c47c15d0d0dd3e764e1ec0a2fbb09e8cbc2d8bf7',
+    );
+  });
+  test('file protocol', async () => {
+    createFixtures({
+      '/test/1.css': `.a {}`,
+    });
+    const mtime = (await stat(getFixturePath('/test/1.css'))).mtime.getTime();
+    expect(await fetchRevision(pathToFileURL(getFixturePath('/test/1.css')).href)).toBe(`mtime:${mtime}`);
+  });
+});
+
+describe('fetchContent', () => {
+  test('http/https protocol', async () => {
+    const protocol = oneOf(['http', 'https']);
+    server.use(rest.get(`${protocol}://example.com/test/1.css`, (_req, res, ctx) => res(ctx.text('.a {}'))));
+    expect(await fetchContent(`${protocol}://example.com/test/1.css`)).toBe('.a {}');
+  });
+  test('file protocol', async () => {
+    createFixtures({
+      '/test/1.css': `.a {}`,
+    });
+    expect(await fetchContent(pathToFileURL(getFixturePath('/test/1.css')).href)).toBe('.a {}');
+  });
+});
+
+test('isURL', () => {
+  expect(isURL('http://example.com')).toBe(true);
+  expect(isURL('https://example.com')).toBe(true);
+  expect(isURL('file:///test/1.css')).toBe(true);
+  expect(isURL('/test/1.css')).toBe(false);
 });
