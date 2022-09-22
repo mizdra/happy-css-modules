3c8a7b78de2d75aacc98af491c6e31feac947f82
mizdra <pp.mizdra@gmail.com>
2022-09-22 15:09:08 +0900

    docs: update example

--- example/02-import/1.css.d.ts
+++ example/02-import/1.css.d.ts
@@ -1,6 +1,8 @@
 declare const styles:
   & Readonly<Pick<(typeof import("./3.css"))["default"], "b">>
+  & Readonly<{ "b": string }>
   & Readonly<Pick<(typeof import("./2.css"))["default"], "a">>
+  & Readonly<{ "a": string }>
 ;
 export default styles;
 //# sourceMappingURL=./1.css.d.ts.map
