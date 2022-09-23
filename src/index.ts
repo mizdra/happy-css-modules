4d5cf1c6f82faa47a734e83afb42db9c24eab1e4
mizdra <pp.mizdra@gmail.com>
2022-09-23 22:51:06 +0900

    feat: export `createDefaultTransformer`, `createDefaultResolver` and `Resolver`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { parseArgv } from './cli.js';
 export { run } from './runner.js';
-export { type Transformer } from './transformer/index.js';
+export { type Transformer, createDefaultTransformer } from './transformer/index.js';
+export { type Resolver, createDefaultResolver } from './resolver/index.js';
