788ed2fa8eb6928187bd85c7c34f3ef837c68d75
mizdra <pp.mizdra@gmail.com>
2022-09-23 19:46:22 +0900

    refactor: remove unused workaround

--- jest.config.mjs
+++ jest.config.mjs
@@ -15,39 +15,6 @@ const config = {
     // Map `./**/xxx.js` to `./**/xxx` (for ESM)
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  globals: {
-    /**
-     * I have a problem with dart-sass not working in Jest (ref: https://github.com/sass/dart-sass/issues/1692).
-     * The community believes that JS Symbols inserted by Jest triggers https://github.com/dart-lang/sdk/issues/47670,
-     * which in turn causes a problem with dart-sass.
-     *
-     * A known workaround for this problem is to switch Jest's test environment to
-     * https://github.com/kayahr/jest-environment-node-single-context. The problem is solved,
-     * perhaps because the Symbol is no longer inserted. However, this test environment is also known
-     * to not work in some situations (ref: https://github.com/kayahr/jest-environment-node-single-context/issues/10).
-     *
-     * So we use another workaround. For reasons unknown, dart-sass does not cause https://github.com/dart-lang/sdk/issues/47670
-     * in the browser environment. So first, we implement a fake `window` object to disguise the browser environment.
-     * (ref: https://github.com/mbullington/node_preamble.dart/blob/3c83ba0887fb64424d3336338da393e4f0eecbf4/lib/preamble.js#L4)
-     *
-     * Next, we implement a fake `location.href`, which is necessary because dart-sass gets base path from `location.href`.
-     *
-     * Finally, we pass a importer when we call `sass.compileString`. Since we are disguising the test environment as a browser,
-     * as is, dart-sass will try to resolve the path with the browser's path resolution algorithm. However, it fails.
-     * Therefore, we need to pass an importer that resolves the path with the Node.js's path resolution algorithm.
-     *
-     * This is a very dirty workaround, but we think it is a good one until https://github.com/dart-lang/sdk/issues/47670
-     * resolves the issue. :)
-     */
-    // NOTE: The workaround for using sass's modern API. happy-css-modules used to use this API.
-    // However, due to the implementation of custom resolvers, we have switched to the legacy API.
-    // Therefore, the workaround is now disabled. See
-    // https://github.com/mizdra/happy-css-modules/issues/65#issuecomment-1229471950 for more information.
-    // window: {},
-    // location: {
-    //   href: 'http://localhost',
-    // },
-  },
   // for ESM
   resolver: join(dir, 'src/test/jest/resolver.cjs'),
 };
--- src/transformer/scss-transformer.ts
+++ src/transformer/scss-transformer.ts
@@ -7,47 +7,6 @@ import type { LegacyResult } from 'sass';
 import type { Transformer, TransformerOptions } from './index.js';
 import { handleImportError } from './index.js';
 
-// const IS_JEST_ENVIRONMENT = process.env.JEST_WORKER_ID !== undefined;
-
-// function verifyJestEnvironment() {
-//   if (
-//     !(
-//       'window' in global &&
-//       'location' in global &&
-//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//       'href' in (global as any).location &&
-//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//       typeof (global as any).location.href === 'string' &&
-//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//       (global as any).location.href.startsWith('http://')
-//     )
-//   ) {
-//     throw new Error(
-//       'To use dart-sass with jest, dummy `global.window` and `global.location.href` must be set. See https://github.com/sass/dart-sass/issues/1692#issuecomment-1229219993 .',
-//     );
-//   }
-// }
-
-// const createImporterForJest: (from: string) => Importer<'async'> = (from) => ({
-//   canonicalize(url) {
-//     // NOTE: The format of `url` changes depending on the import source.
-//     //
-//     // - When `from === '/test/1.scss'` and `@import './2.scss'` in `/test/1.scss` is resolved, `url === '2.scss'`.
-//     // - When `from === '/test/1.scss'` and `@import './3.scss'` in `/test/2.scss` is resolved, `url === 'file:///test/3.scss'`.
-//     //
-//     // That is, the paths of @import statements written to the `from` file is passed through unresolved,
-//     // but paths written to other files is passed through resolved to absolute paths.
-//     return new URL(url, pathToFileURL(from));
-//   },
-//   async load(canonicalUrl) {
-//     return {
-//       contents: await readFile(fileURLToPath(canonicalUrl.href), 'utf8'),
-//       syntax: 'scss',
-//       sourceMapUrl: canonicalUrl,
-//     };
-//   },
-// });
-
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 async function renderSass(sass: typeof import('sass'), source: string, options: TransformerOptions) {
   return new Promise<LegacyResult>((resolve, reject) => {
@@ -82,13 +41,5 @@ export const createScssTransformer: () => Transformer = () => {
     sass ??= (await import('sass').catch(handleImportError('sass'))).default;
     const result = await renderSass(sass, source, options);
     return { css: result.css.toString(), map: result.map!.toString(), dependencies: result.stats.includedFiles };
-
-    // if (IS_JEST_ENVIRONMENT) verifyJestEnvironment();
-    // const result = await sass.default.compileStringAsync(source, {
-    //   url: pathToFileURL(from),
-    //   sourceMap: true,
-    //   importers: IS_JEST_ENVIRONMENT ? [createImporterForJest(from)] : [],
-    // });
-    // return { css: result.css, map: result.sourceMap!, dependencies: result.loadedUrls };
   };
 };
