e61b59f7820f94a75ef3dbbfc585be3c44be6b49
mizdra <pp.mizdra@gmail.com>
2022-09-23 22:54:48 +0900

    feat: export `LocalsConvention`, `TransformerOptions`, `TransformResult` and `ResolverOptions`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 export { parseArgv } from './cli.js';
-export { run } from './runner.js';
-export { type Transformer, createDefaultTransformer } from './transformer/index.js';
-export { type Resolver, createDefaultResolver } from './resolver/index.js';
+export { run, type LocalsConvention } from './runner.js';
+export {
+  type Transformer,
+  type TransformerOptions,
+  type TransformResult,
+  createDefaultTransformer,
+} from './transformer/index.js';
+export { type Resolver, type ResolverOptions, createDefaultResolver } from './resolver/index.js';
