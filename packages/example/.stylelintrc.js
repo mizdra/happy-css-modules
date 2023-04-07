114d146998fe79388a5827023440b88e38300422
mizdra <pp.mizdra@gmail.com>
2023-04-07 15:51:52 +0900

    initial implementation

--- packages/example/.stylelintrc.js
+++ packages/example/.stylelintrc.js
@@ -1,7 +1,14 @@
+// @ts-check
+
 const { resolve } = require('path');
 
+/** @type {import('stylelint').Config} */
 module.exports = {
   plugins: ['stylelint-happy-css-modules'],
+  overrides: [
+    { files: ['**/*.scss'], customSyntax: require('postcss-scss') },
+    { files: ['**/*.less'], customSyntax: require('postcss-less') },
+  ],
   rules: {
     'happy-css-modules/no-unused-selectors': [true, { tsConfigFilePath: resolve(__dirname, 'tsconfig.json') }],
   },
