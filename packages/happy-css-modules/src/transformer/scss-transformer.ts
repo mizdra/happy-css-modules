daa7f95f0f996cb7f6642f17e9d7d4d7f284b462
mizdra <pp.mizdra@gmail.com>
2023-06-13 00:24:11 +0900

    rename

--- packages/happy-css-modules/src/transformer/scss-transformer.ts
+++ packages/happy-css-modules/src/transformer/scss-transformer.ts
@@ -22,8 +22,8 @@ function promisifySassRender(sass: typeof import('sass')) {
 export const createScssTransformer: () => Transformer = () => {
   let sass: typeof import('sass');
   return async (source, options) => {
-    const importedSass = await import('sass').catch(handleImportError('sass'));
-    sass ??= importedSass.default ?? importedSass;
+    const sassModule = await import('sass').catch(handleImportError('sass'));
+    sass ??= sassModule.default ?? sassModule;
     const render = promisifySassRender(sass);
     const result = await render({
       data: source,
