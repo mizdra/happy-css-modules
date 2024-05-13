3643e40466d027d7d4255bdc02ce4af15d5679f1
mizdra <pp.mizdra@gmail.com>
2024-05-14 01:13:12 +0900

    fix lint errors

--- packages/example/07-typescript-plugin/Card.tsx
+++ packages/example/07-typescript-plugin/Card.tsx
@@ -1,7 +1,7 @@
 export function Card({ children }: { children: React.ReactNode }) {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.content}>{children}</div>
+    <div className="styles.wrapper">
+      <div className="styles.content">{children}</div>
     </div>
   );
 }
